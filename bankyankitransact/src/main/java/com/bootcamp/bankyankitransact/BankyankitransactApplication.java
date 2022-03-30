c2a2b57c2c734f10131b7e8c99a86bc9477959bb
pwhuanco <pedrohuanco@gmail.com>
2022-03-30 17:41:57 -0500

    primera subida

---
+++ bankyankitransact/src/main/java/com/bootcamp/bankyankitransact/BankyankitransactApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankyankitransact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankyankitransactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankyankitransactApplication.class, args);
+	}
+
+}
---
+++ bankyankitransact/src/test/java/com/bootcamp/bankyankitransact/BankyankitransactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankyankitransact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankyankitransactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
